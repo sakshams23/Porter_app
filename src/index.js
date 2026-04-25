bb7a82805497074b341182b15690baf42eba14f8
Saksham Soni <sonisaksham20208@gmail.com>
2026-04-25 16:27:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
