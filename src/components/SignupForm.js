88a6f679996b491b480d6088472ca12c28a90371
Saksham Soni <sonisaksham20208@gmail.com>
2026-04-25 16:29:50 +0530

    Update SignupForm.js

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import {toast} from "react-hot-toast"
 import { useNavigate } from 'react-router-dom';
 
 
